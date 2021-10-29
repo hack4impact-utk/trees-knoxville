44f2e0ef2435fd3ab630d092f168a9d551268c87
andlrutt <andlrutt@vols.utk.edu>
2021-10-29 14:48:43 -0400

    defined Tree type

--- utils/types.ts
+++ utils/types.ts
@@ -1,4 +1,13 @@
 
 export interface Tree {
-    
+    _id?: string;
+    species?: string;
+    age?: number;
+    coordinates?: {
+        latitude: number,
+        longitude: number,
+    };
+    adopted?: boolean;
+    watering?: boolean;
+    pruning?: boolean;
 }
