db313f3c81a83ce7633cb3d34d2d30dd7284a9af
andlrutt <andlrutt@vols.utk.edu>
2021-11-01 12:04:40 -0400

    Added server/index.ts to establish DB connection

---
+++ server/index.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import urls from "../utils/urls";
+
+// load the models before we start using them or else they wont
+// be defined and we get a MissingSchemaError
+require("./models/Tree");
+
+export default async () => {
+    if (mongoose.connections[0].readyState) return;
+    const DB_URL: string = urls.dbUrl;
+
+    await mongoose
+        .connect(DB_URL)
+        .catch((error: any) => {
+            console.error("Database connection failed.");
+            throw error;
+        });
+};
