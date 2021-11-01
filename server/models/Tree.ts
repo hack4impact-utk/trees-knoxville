653f7e0d4ff8a60000f331f5dfbafde1ff7910b2
andlrutt <andlrutt@vols.utk.edu>
2021-11-01 11:44:56 -0400

    completed tree schema

--- server/models/Tree.ts
+++ server/models/Tree.ts
@@ -2,8 +2,43 @@ import { model, models, Model, Schema, Document } from "mongoose";
 import { Tree } from "../../utils/types";
 
 export const TreeSchema = new Schema({
-    
 
+    species: {
+        type: String,
+        required: false,
+    },
+
+    age: {
+        type: Number,
+        required: false,
+    },
+
+    coordinates: {
+        latitude: {
+            type: Number,
+            required: false,
+        },
+
+        longitude: {
+            type: Number,
+            required: false,
+        }
+    },
+
+    adopted: {
+        type: Boolean,
+        required: false,
+    },
+
+    watering: {
+        type: Boolean,
+        required: false,
+    },
+
+    pruning: {
+        type: Boolean,
+        required: false,
+    },
 });
 
 export interface EventDocument extends Omit<Tree, "_id">, Document {}
