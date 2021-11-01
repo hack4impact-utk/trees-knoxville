92bd357cb1d8147de9e2493975ffc17d44ac9ef1
Nolan Askew <nolanaskew@gmail.com>
2021-11-01 11:15:26 -0400

    created map page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,10 @@ import React from "react";
 
 export default function HomePage() {
     return (
-    <h1>Welcome to Trees Knoxville!</h1>
+    <div> 
+        <title>Trees Knoxville | Map</title>
+        <h1>Welcome to Trees Knoxville!</h1>
+        <br></br> Map Goes Here
+    </div>
     );
 }
