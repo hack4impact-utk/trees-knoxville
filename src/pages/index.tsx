28b5a8af812faf5fb8efa18d90b7786f83769542
Nolan Askew <nolanaskew@gmail.com>
2021-11-09 10:48:03 -0500

    included title in head tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,9 @@ import React from "react";
 export default function HomePage() {
     return (
     <div> 
+        <head>
         <title>Trees Knoxville | Map</title>
+        </head>
         <h1>Welcome to Trees Knoxville!</h1>
         <br></br> Map Goes Here
     </div>
