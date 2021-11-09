f1a822457a4dfed3dd24a2647e18826af8cc3629
Reagan (Alex) Austin <71673490+raustin9@users.noreply.github.com>
2021-11-09 18:36:12 -0500

    implement updateTree & getTreesByVisibilityStatus (#5)
    Co-authored-by: Trevor Mangrum <25257772+trevormangrum@users.noreply.github.com>

--- server/actions/Tree.ts
+++ server/actions/Tree.ts
@@ -15,3 +15,27 @@ import { Tree } from "../../utils/types";
 
     await TreeSchema.create(tree);
 };
+
+/**
+ * @param queryTree tree object containing only the ID
+ * @param newTree new tree to replace the old one
+ */
+export const updateTree = async function (queryTree: Tree, newTree: Tree) {
+    await mongoDB();
+    const options = { useFindAndModify: true };
+
+    // update only updates a single item since we specify the _id field
+    // in queryTree. newTree replaces the old object.
+    await TreeSchema.findOneAndUpdate(queryTree, newTree, options);
+}
+
+/**
+ * 
+ * @param visibilityStatus true for published false for unpublished
+ * @returns trees with given visibilityStatus
+ */
+export const getTreesByVisibilityStatus = async function (visibilityStatus: boolean) {
+    await mongoDB();
+    return await TreeSchema.find({published:visibilityStatus});
+}
+
