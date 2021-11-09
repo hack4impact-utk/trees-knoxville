46ee53f90a9cbe4e89dd971f81d1f8128608ec26
Nolan Askew <nolanaskew@gmail.com>
2021-11-09 13:30:34 -0500

    created admin pages

---
+++ src/pages/admin/dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function AdminDashboard() {
+    return (
+    <div>
+        <head>
+            <title>Admin Dashboard | Trees Knoxville</title>
+        </head>
+        <h1>Admin Dashboard Page</h1>
+    </div>
+    );
+}
