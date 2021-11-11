1679322ef7fb528cd1c5600a0b54597b45a5d5e3
Andrew Rutter <andlrutt@vols.utk.edu>
2021-11-10 21:06:26 -0500

    outline for post for trees

---
+++ src/pages/api/admin/trees/index.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Tree } from "../../../../../utils/types"
+import { addTree, updateTree } from "../../../../../server/actions/Tree"
+import { format } from "prettier";
+
+
+
+// @route   POST /api/events/admin/trees - Create a tree from form data
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        if (req.method === "POST") {
+            const form: Tree = JSON.parse(req.body);
+
+            
+        }
+    }
+}
