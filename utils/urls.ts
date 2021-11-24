34d5a746e5cf6f4454220db66ee45d7745b979b0
Andrew Rutter <andlrutt@vols.utk.edu>
2021-11-24 13:40:51 -0500

    added page urls to utils/urls

--- utils/urls.ts
+++ utils/urls.ts
@@ -1,7 +1,14 @@
 
 export default {
     dbUrl: process.env.MONGO_DB ?? "mongodb://localhost:27017",
+    pages: {
+        index: "/",
+        adminHome: "/admin",
+        addTree: "/admin/create",
+        adminTrees: "/admin/trees",
+        updateTree: (treeId: string) => `/admin/trees/${treeId}`,
+    },
     api: {
         trees: "/api/admin/trees"
-    }
+    },
 }
