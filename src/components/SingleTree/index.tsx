1ed7bdd3720ab370ae8d12a8dfca51a19ab7e689
Andrew Rutter <andlrutt@vols.utk.edu>
2021-11-26 17:52:16 -0500

    updated SingleTree to properly display tree props

--- src/components/SingleTree/index.tsx
+++ src/components/SingleTree/index.tsx
@@ -16,11 +16,11 @@ const SingleTree: NextPage<Props> = ({ tree }) => {
             <span>ObjectID={tree._id}</span><br/>
             <span>Species={tree.species}</span><br/>
             <span>Coordinates={tree.coordinates?.latitude}, {tree.coordinates?.longitude}</span><br/>
-            <span>Adpoted={tree.adopted}</span><br/>
-            <span>Watering={tree.watering}</span><br/>
-            <span>Pruning={tree.pruning}</span><br/>
-            <span>Date Planted={tree.datePlanted}</span><br/>
-            <span>published={tree.published}</span><br/>
+            <span>Adpoted={tree.adopted == true ? "true" : "false"}</span><br/>
+            <span>Watering={tree.watering == true ? "true" : "false"}</span><br/>
+            <span>Pruning={tree.pruning == true ? "true" : "false"}</span><br/>
+            <span>Date Planted={tree.datePlanted?.toString().split("T")[0]}</span><br/>
+            <span>published={tree.published == true ? "true" : "false"}</span><br/>
         </div>
   
     );
