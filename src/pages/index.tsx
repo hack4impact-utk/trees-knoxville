09672b728348118f0b38dedecfefb04f2d71516d
Andrew Rutter <andlrutt@vols.utk.edu>
2022-01-12 15:30:50 -0500

    generated props for MapContainer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import AllTrees from "src/components/AllTrees";
 import UpsertTreeForm from "src/components/UpsertTreeForm";
 import MapContainer from "src/components/Map";
+import { GetStaticPropsContext, NextPage } from "next";
+import { Tree } from "utils/types";
+import { getTreesByVisibilityStatus } from "server/actions/Tree";
 
+interface Props {
+    trees: Tree[],
+}
 
-export default function HomePage() {
+const HomePage: NextPage<Props> = ({ trees }) => {
     return (
     <div> 
         <head>
@@ -13,10 +18,33 @@ export default function HomePage() {
         <h1>Welcome to Trees Knoxville!</h1>
         <br></br>
         <div>
-            <MapContainer />
+            <MapContainer trees={trees} />
             <br /><br />
             <UpsertTreeForm />
         </div>  
     </div>
     );
 }
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+    try {
+        // only puts published trees on the map
+        const trees: Tree[] = await getTreesByVisibilityStatus(true);
+        
+        return {
+            props: {
+                trees: (JSON.parse(JSON.stringify(trees))) as Tree[],
+            },
+        };
+    }
+    catch (error) {
+        console.log(error);
+        return {
+            props: {
+                trees: []
+            },
+        }
+    }
+}
+
+export default HomePage;
