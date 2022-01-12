a2213e1175cc960aa274a78f3a160332e21fa49a
Andrew Rutter <andlrutt@vols.utk.edu>
2022-01-12 15:30:17 -0500

    added markers to map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,19 +1,23 @@
-import Tree from "utils/types";
+import { Tree } from "utils/types";
 import Map from "google-map-react";
 import styles from "../Map.module.css"
+import { FunctionComponent } from "react";
+import Marker from "src/components/Marker"
 
 interface Trees {
     trees: Tree[];
 }
 
 
-const MapContainer: FunctionComponent<Trees> = ({trees}) => {
+const MapContainer: FunctionComponent<Trees> = ({ trees }) => {
     return (
         <div className = {styles.Map} >
             <Map
             bootstrapURLKeys={{key: process.env.NEXT_PUBLIC_MAPS_API_KEY ?? ''}}
             defaultCenter={{ lat: 35.96, lng: -83.92 }}
             defaultZoom={11}>
+                {trees && trees.map((tree: Tree) => 
+                    <Marker lat={Number(tree.coordinates?.latitude)} lng={Number(tree.coordinates?.longitude)} />)}
             </Map>
         </div>
     )
