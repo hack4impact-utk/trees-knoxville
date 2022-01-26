e3d4fff81db253d1a7f17949338c1a9260a1ba14
Andrew Rutter <andlrutt@vols.utk.edu>
2022-01-26 14:01:40 -0500

    added error checks for lat and long values

--- src/pages/api/admin/trees/[treeId].ts
+++ src/pages/api/admin/trees/[treeId].ts
@@ -12,6 +12,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             const id = req.query.treeId as string;
             const tree: Tree = JSON.parse(req.body);
 
+            // ensures coordinates are valid
+            const numberLat = Number(tree.coordinates?.latitude);
+            const numberLong = Number(tree.coordinates?.longitude);
+            if (!numberLat ||  numberLat < -90 || numberLat > 90 ) {
+                throw Error ("Invalid Latitude");
+            }
+            if (!numberLong || numberLong < -180 || numberLong > 180) {
+                throw Error ("Invalid Longitude");
+            }
+
             await updateTree({ _id: id }, tree);
 
             res.status(200).json({
