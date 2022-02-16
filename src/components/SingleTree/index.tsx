e0439a865ff07c5ec786235254d6c1b0092c4559
Andrew Rutter <andlrutt@vols.utk.edu>
2022-02-16 10:51:56 -0500

    updated how dates are presented

--- src/components/SingleTree/index.tsx
+++ src/components/SingleTree/index.tsx
@@ -9,18 +9,33 @@ interface Props {
 
 const SingleTree: NextPage<Props> = ({ tree }) => {
 
-    
+    function GetFormattedDate(date: Date) {
+
+        // in case the tree does not have a date
+        if (!date) return "";
+
+        // YYYY-MM-DD format
+        const dateString = date.toString().split("T")[0];
+
+        const [year, month, day] = dateString.split('-');
+
+        // MM/DD/YYYY format
+        const formattedDate = [month, day, year].join('/');
+
+        return formattedDate;
+    }
 
     return (
     <div className={styles.container}>
         <div className={styles.placeholderImage}></div>
         <div className={styles.row}>   
-            <span className={`${styles.info} ${styles.mobileInfo1}`}>{tree.datePlanted?.toString().split("T")[0]}</span>
-            <span className={`${styles.info} ${styles.mobileInfo2}`}>TREE_NAME</span>
-            <span className={`${styles.info} ${styles.mobileInfo3}`}><span className={styles.hiddenText}>Volunteer Forester:&#160;</span>FORESTER</span>
-            <span className={`${styles.info} ${styles.mobileInfo4}`}>{tree.coordinates?.latitude}, {tree.coordinates?.longitude}</span><br/>
-            <span className={`${styles.info} ${styles.mobileInfo5}`}>NUMBER?</span>
-            <span className={`${styles.info} ${styles.mobileInfo6}`}>{tree.species}</span>
+            <div className={`${styles.info} ${styles.mobileInfo2}`}>
+                <span className={`${styles.hiddenText}`}>{`${tree.datePlanted ? "Planted on" : ""}`}&nbsp;</span>
+                {/*<span>{tree.datePlanted?.toString().split("T")[0]}</span>*/}
+                <span>{GetFormattedDate(tree.datePlanted!)}</span>
+            </div>
+            <span className={`${styles.info} ${styles.mobileInfo3}`}>{tree.coordinates?.latitude}, {tree.coordinates?.longitude}</span>
+            <span className={`${styles.info} ${styles.mobileInfo1}`}>{tree.species}</span>
         </div>
     </div>
     );
