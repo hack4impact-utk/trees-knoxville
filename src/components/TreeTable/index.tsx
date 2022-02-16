265152de520fa91af56111d09940e3a2f6b91164
Andrew Rutter <andlrutt@vols.utk.edu>
2022-02-16 10:52:38 -0500

    made table elements clickable

--- src/components/TreeTable/index.tsx
+++ src/components/TreeTable/index.tsx
@@ -11,19 +11,22 @@ interface Props {
 }
 
 const TreeTable: NextPage<Props> = ({ trees }) => {
+
+    // reroutes to specific tree page
+    const onClick = (treeId: string) => {
+        window.location.replace(urls.pages.updateTree(treeId));
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.headerRow}>
                 <span className={styles.headerItem}>Date</span><br/>
-                <span className={styles.headerItem}>Name of Tree/Collection</span>
-                <span className={styles.headerItem}>User</span>
                 <span className={styles.headerItem}>Location</span><br/>
-                <span className={styles.headerItem}># of Trees</span>
                 <span className={styles.headerItem}>Type of Tree</span><br/>
             </div>
             {trees && trees.map((tree: Tree) => {
             return (
-                <div key={tree._id}>
+                <div key={tree._id} style={{cursor: "pointer"}} onClick={() => onClick(tree._id!)}>
                     <SingleTree tree={tree} />
                     {/*<button type="button" onClick={() => onClick(tree._id!)}>Edit</button>*/}
                     <div className={styles.line} />
