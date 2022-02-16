4223757c37960c3e0877224f2f3df3286f067592
Andrew Rutter <andlrutt@vols.utk.edu>
2022-02-16 10:50:30 -0500

    got rid of non-component related items

--- src/pages/admin/trees/index.tsx
+++ src/pages/admin/trees/index.tsx
@@ -23,41 +23,11 @@ const AdminTrees: NextPage<Props> = ({ trees }) => {
         <head>
             <title>Admin Trees | Trees Knoxville</title>
         </head>
-        <h1 className="hello">Admin Trees Page</h1>
+        <h1>Admin Trees Page</h1>
         
-        <div className="table">
-            <div className="filterRow">
-                <button className="filterButton"><MdOutlineSort className="icon"/>Filter by Most Recent</button>
-                <button className="filterButton"><MdWatchLater className="icon"/>Past Week</button>
-            </div>
+        <div>
             <TreeTable trees={trees}/>
         </div>
-        <style jsx global>{`
-            
-            .filterRow {
-                display: flex;
-                justify-content: right;
-            }
-            
-            .filterButton {
-                display: flex;
-                align-items: center;
-                cursor: pointer;
-                font-size: 20px;
-                padding: 5px 10px 5px 10yapx;
-                margin: 0 0 10px 20px;
-                background-color: white;
-                border: 0.5px solid #6F6F6F;
-                box-sizing: border-box;
-                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
-                border-radius: 5px;
-            }
-
-            .icon {
-                padding-right: 4px;
-                color: #219076;
-            }
-      `}</style> 
     </div>
     );
 }
