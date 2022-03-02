cb95493e1b0b51ea6f174b729e45b1320694fcba
Andrew Rutter <andlrutt@vols.utk.edu>
2022-03-02 14:14:34 -0500

    updated userTable to actually have a table

--- src/components/UserTable/index.tsx
+++ src/components/UserTable/index.tsx
@@ -1,13 +1,26 @@
-import { NextPage } from "next";
 import React from "react";
+import { NextPage } from "next";
+import styles from "src/components/UserTable/UserTable.module.scss"
 
 
+interface Props {
+    users: [];
+}
 
-
-const UserTable = () => {
+const UserTable: NextPage<Props> = ({ users }) => {
     return (
-        <div>howdy</div>
-    )
+        <div>
+            <div className={styles.headerRow}>
+                <div className={styles.placeholderDiv}></div>
+                <span className={styles.headerItem}>Date</span><br/>
+                <span className={styles.headerItem}>Location</span><br/>
+                <span className={styles.headerItem}>Type of Tree</span><br/>
+            </div>
+            <div className={styles.line} />
+            <span></span>
+        
+        </div>
+    );
     
 }
 
