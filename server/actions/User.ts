59a559179a28794c38ef7fe1a4febff45e1fafa5
Andrew Rutter <andlrutt@vols.utk.edu>
2022-03-05 19:27:20 -0500

    updateUser function

--- server/actions/User.ts
+++ server/actions/User.ts
@@ -1,7 +1,7 @@
 import mongoDB from "server/index";
 import TreeSchema from "server/models/Tree";
 import { Types } from "mongoose";
-import { Tree } from "utils/types";
+import { Tree, User } from "utils/types";
 import TreeModel from "server/models/Tree";
 
 
@@ -9,16 +9,16 @@ import TreeModel from "server/models/Tree";
  * @returns all users in auth0
  */
  export const getUsers = async function() {
-  var axios = require("axios").default;
+  const axios = require("axios").default;
 
-  var options = {
+  const options = {
     method: 'GET',
     url: `https://${process.env.AUTH0_DOMAIN}/api/v2/users`,
     params: {q: 'name:*', search_engine: 'v3'},
     headers: {authorization: `Bearer ${process.env.AUTH0_MGMT_API_ACCESS_TOKEN}`}
   };
 
-  const response = await axios.request(options)
+  const response = await axios.request(options);
   return response.data;
 }
 
@@ -27,15 +27,36 @@ import TreeModel from "server/models/Tree";
  * @returns a single user
  */
 export const getUser = async function(id: string) {
-  var axios = require("axios").default;
+  const axios = require("axios").default;
 
-  var options = {
+  const options = {
     method: 'GET',
     url: `https://${process.env.AUTH0_DOMAIN}/api/v2/users/${id}`,
     headers: {authorization: `Bearer ${process.env.AUTH0_MGMT_API_ACCESS_TOKEN}`}
   };
 
-  const response = await axios.request(options)
+  const response = await axios.request(options);
 
   return response.data;
+}
+
+/**
+ * @param updatedUser A user with updated fields
+ */
+export const updateUser = async function(updatedUser: User) {
+  const axios = require("axios").default;
+
+  const options = {
+    method: 'PATCH',
+    url: `https://${process.env.AUTH0_DOMAIN}/api/v2/users/${updatedUser.user_id}`,
+    headers: {authorization: `Bearer ${process.env.AUTH0_MGMT_API_ACCESS_TOKEN}`},
+    body: {
+      name: updatedUser.name,
+      email: updatedUser.email,
+      phone_number: updatedUser.phone,
+      trees: updatedUser.trees,
+    },
+  };
+
+  await axios.request(options);
 }
