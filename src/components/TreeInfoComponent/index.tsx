633a36597f3103ca5904d19c2b6a230403ecda35
Trevor Mangrum <mangrumtrevor@gmail.com>
2022-03-30 17:17:41 -0400

    Fixes issues from PR

--- src/components/TreeInfoComponent/index.tsx
+++ src/components/TreeInfoComponent/index.tsx
@@ -7,7 +7,6 @@ interface Props {
     setDisplayingTree: Dispatch<SetStateAction<boolean>>;
 }
 const TreeInfoComponent: React.FC<Props> = ({tree, displayingTree, setDisplayingTree}) => {
-    console.log(tree);
     const date = tree && new Date(tree.datePlanted as Date).toLocaleString("en-US", {
         timeZone: "UTC",
         weekday: "long",
@@ -18,10 +17,10 @@ const TreeInfoComponent: React.FC<Props> = ({tree, displayingTree, setDisplaying
     return (
         <div className={`${styles.treeInfo} ${displayingTree ? styles.open : styles.closed}`}>
             <button className={styles.closeButton} onClick={() => setDisplayingTree(false)}>Close Window</button>
-            <img src={tree && tree.image && tree.image.url} alt={tree.species} className={styles.treeImage} />
+            <img src={tree && tree.image ? tree.image.url : "/defaultTreeImage.png"} alt={tree && tree.species} className={styles.treeImage} />
             <div className={styles.treeText}>
                 <h3>Coordinates</h3>
-                <p>{tree && tree.coordinates?.latitude}, {tree && tree.coordinates.longitude}</p>
+                <p>{tree && tree.coordinates?.latitude}, {tree && tree.coordinates?.longitude}</p>
                 <h3>Date Planted</h3>
                 <p>{tree && date}</p>
                 <h3>Species</h3>
