4339ab3beedc7e3f54fb2ceea6dd832fbb407868
Andrew Rutter <andlrutt@vols.utk.edu>
2022-04-12 15:21:36 -0400

    merged w andlrutt/TKW-58-filter-trees-search-bar

--- src/pages/admin/trees/index.tsx
+++ src/pages/admin/trees/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Tree } from "utils/types";
 import { GetStaticPropsContext, NextPage } from "next";
 import { getTrees } from "server/actions/Tree";
@@ -24,6 +24,33 @@ const AdminTrees: NextPage<Props> = ({ trees }) => {
         }
     };
 
+    // maintains an array of all trees being displayed
+    const [filterTrees, setFilterTrees] = useState(trees);
+
+    const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // applies a filter to trees[] based on the current value of the input box
+        addFilter(e.target.value)
+    }
+
+    const addFilter = (query: string) => {
+        // if there is a query (input field isn't blank), apply a filter. Otherwise, remove all filters
+        if (query) {
+            setFilterTrees(trees.filter((tree) => filter(tree, query)));
+        }
+        else {
+            removeFilter();
+        }
+    }
+
+    const removeFilter = () => {
+        // trees[] holds all trees in the database, so this removes any filters
+        setFilterTrees(trees);
+    };
+
+    const filter = (tree: Tree, query: string) => {
+        return tree.species?.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    }
+
 
     return (
     <div>    
@@ -45,7 +72,8 @@ const AdminTrees: NextPage<Props> = ({ trees }) => {
             <input type="date"></input>
         </div>
         <div>
-            <TreeTable trees={trees}/>
+            <input onChange={handleQueryChange} placeholder="Search by species name"></input>
+            <TreeTable trees={filterTrees}/>
         </div>
     </div>
     );
