0075102fc18e4e886c15fd22388a064ba3c383fd
Andrew Rutter <andlrutt@vols.utk.edu>
2022-04-12 15:09:06 -0400

    filter input type dropdown

--- src/pages/admin/trees/index.tsx
+++ src/pages/admin/trees/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Tree } from "utils/types";
 import { GetStaticPropsContext, NextPage } from "next";
 import { getTrees } from "server/actions/Tree";
-import urls from "utils/urls";
 import TreeTable from "src/components/TreeTable";
 
 interface Props {
@@ -11,6 +10,21 @@ interface Props {
 
 const AdminTrees: NextPage<Props> = ({ trees }) => {
 
+    const handleDropdownChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const filterType = e.target.value;
+        if (filterType === "age") {
+
+        }
+        else if (filterType === "date") {
+
+        }
+        // removes filter and clears age and date input fields
+        else {
+
+        }
+    };
+
+
     return (
     <div>    
         <head>
@@ -18,11 +32,17 @@ const AdminTrees: NextPage<Props> = ({ trees }) => {
         </head>
         <h1>Admin Trees Page</h1>
         <div>
-            <select name="filterType">
-                <option value="none" selected>-- Filter by --</option>
+            <select onChange={handleDropdownChange} name="filterType">
+                <option value="none" selected>--- Filter by ---</option>
                 <option value="age">Age</option>
                 <option value="date">Date</option>
             </select>
+
+            <input type="number" placeholder="start age"></input>
+            <input type="number" placeholder="end age"></input>
+
+            <input type="date"></input>
+            <input type="date"></input>
         </div>
         <div>
             <TreeTable trees={trees}/>
