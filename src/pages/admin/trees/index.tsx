ce143ce7f10772b4654175ce066431f8a3fa1538
Andrew Rutter <andlrutt@vols.utk.edu>
2022-04-13 10:50:44 -0400

    revamped species filter, added date and age filter

--- src/pages/admin/trees/index.tsx
+++ src/pages/admin/trees/index.tsx
@@ -4,6 +4,16 @@ import { GetStaticPropsContext, NextPage } from "next";
 import { getTrees } from "server/actions/Tree";
 import TreeTable from "src/components/TreeTable";
 
+
+interface stateInterface {
+    filterType: string,
+    minAge: number,
+    maxAge: number,
+    minDate: Date,
+    maxDate: Date,
+    speciesName: string,
+}
+
 interface Props {
     trees: Tree[],
 }
@@ -11,46 +21,89 @@ interface Props {
 const AdminTrees: NextPage<Props> = ({ trees }) => {
 
     const handleDropdownChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const filterType = e.target.value;
-        if (filterType === "age") {
-
+        if (e.target.value === "none") {
+            // empty out those input containers!
         }
-        else if (filterType === "date") {
 
-        }
-        // removes filter and clears age and date input fields
-        else {
-
-        }
+        values.filterType = e.target.value;
     };
 
     // maintains an array of all trees being displayed
     const [filterTrees, setFilterTrees] = useState(trees);
+    const [values, setValues] = React.useState<stateInterface>({} as stateInterface);
+    
 
-    const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        // applies a filter to trees[] based on the current value of the input box
-        addFilter(e.target.value)
-    }
-
-    const addFilter = (query: string) => {
+    const addSpeciesFilter = (speciesName: string) => {
         // if there is a query (input field isn't blank), apply a filter. Otherwise, remove all filters
-        if (query) {
-            setFilterTrees(trees.filter((tree) => filter(tree, query)));
+        if (speciesName) {
+            setFilterTrees(trees.filter((tree) => speciesFilter(tree, speciesName)));
         }
         else {
             removeFilter();
         }
     }
 
+    const addAgeFilter = (minAge: number, maxAge: number, speciesName: string) => {
+        setFilterTrees(trees.filter((tree) => ageFilter(tree, minAge, maxAge, speciesName)));
+    };
+
+    const addDateFilter = (minDate: Date, maxDate: Date, speciesName: string) => {
+        setFilterTrees(trees.filter((tree) => dateFilter(tree, minDate, maxDate, speciesName)));
+    };
+
+    const speciesFilter = (tree: Tree, speciesName: string) => {
+        return tree?.species?.toLowerCase().indexOf(speciesName.toLowerCase()) !== -1;
+    };
+
+    const ageFilter = (tree: Tree, minAge: number, maxAge: number, speciesName: string) => {
+        return tree?.age! >= minAge && tree?.age! <= maxAge && speciesFilter(tree, speciesName);
+    };
+
+    const dateFilter = (tree: Tree, minDate: Date, maxDate: Date, speciesName: string) => {
+        // something with this date comparison...
+        return new Date(tree?.datePlanted!) >= minDate && new Date(tree?.datePlanted!) <= maxDate && speciesFilter(tree, speciesName)
+    };
+
     const removeFilter = () => {
         // trees[] holds all trees in the database, so this removes any filters
         setFilterTrees(trees);
     };
 
-    const filter = (tree: Tree, query: string) => {
-        return tree.species?.toLowerCase().indexOf(query.toLowerCase()) !== -1;
-    }
 
+    const onChange = (event: React.SyntheticEvent) => {
+        event.persist();
+        const target = event.target as HTMLInputElement;
+        setValues(values => ({...values, [target.name]: target.value}));
+        
+        
+        
+        if (values.filterType === "age") {
+            if (target.name === "minAge") {
+                addAgeFilter(Number(target.value), values.maxAge || 1000, values.speciesName);
+            }
+            else if (target.name === "maxAge") {
+                addAgeFilter(values.minAge, Number(target.value) || 1000, values.speciesName);
+            }
+            else {
+                addAgeFilter(values.minAge, values.maxAge || 1000, target.value);
+            }
+        }
+        else if (values.filterType === "date") {
+            if (target.name === "minDate") {
+                addDateFilter(new Date(target.value) || 0, values.maxDate || new Date(), values.speciesName) ;
+            }
+            else if (target.name === "maxDate") {
+                addDateFilter(values.minDate || 0, new Date(target.value) || new Date(), values.speciesName);
+            }
+            else {
+                addDateFilter(values.minDate || 0, values.maxDate || new Date(), target.value);
+            }
+        }
+        else {
+            // species name changed, and no other filters should be applied
+            addSpeciesFilter(target.value);
+        }
+    }
 
     return (
     <div>    
@@ -65,14 +118,14 @@ const AdminTrees: NextPage<Props> = ({ trees }) => {
                 <option value="date">Date</option>
             </select>
 
-            <input type="number" placeholder="start age"></input>
-            <input type="number" placeholder="end age"></input>
+            <input name="minAge" type="number" placeholder="start age" onChange={onChange}></input>
+            <input name="maxAge" type="number" placeholder="end age" onChange={onChange}></input>
 
-            <input type="date"></input>
-            <input type="date"></input>
+            <input type="date" onChange={onChange}></input>
+            <input type="date" onChange={onChange}></input>
         </div>
         <div>
-            <input onChange={handleQueryChange} placeholder="Search by species name"></input>
+            <input name="speciesName" placeholder="Search by species name" onChange={onChange}></input>
             <TreeTable trees={filterTrees}/>
         </div>
     </div>
