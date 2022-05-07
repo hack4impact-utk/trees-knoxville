b775e4fefbb371e39a991ef64e6ff628655ca856
Andrew Rutter <andlrutt@vols.utk.edu>
2022-05-07 19:25:55 -0400

    /api/contentful/[entryId] DELETE method route

--- src/pages/api/contentful/[entryId].ts
+++ src/pages/api/contentful/[entryId].ts
@@ -1,15 +1,28 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { createTreeEntry, getTreeEntry } from "server/actions/Contentful";
-import { ContentfulEntry } from "utils/types";
+import { deleteEntryByID, getTreeEntry } from "server/actions/Contentful";
+import { Tree } from "utils/types";
 
 
 
-// @route   GET   /api/contentful/[entryId] - Returns entry from contentful with the given Id
+// @route   GET    /api/contentful/[entryId] - Returns entry from contentful with the given Id
+// @route   DELETE /api/contentful/[entryId] - Deletes entry from contentful with the given Id
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         if (req.method === "GET") {
             const entry = await getTreeEntry(req.query.entryId as string);
-            return entry;
+            res.status(200).json({
+                success: true,
+                payload: entry,
+            });
+        }
+
+        if (req.method === "DELETE") {
+            const tree: Tree = JSON.parse(req.body);
+            await deleteEntryByID(req.query.entryId as string, tree);
+            res.status(200).json({
+                success: true,
+                payload: {}
+            });
         }
     } catch (error) {
         console.log(error);
